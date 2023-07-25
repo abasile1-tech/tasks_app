a7ceb7b44c566af73dc8d1d8ee65ea8ba9189e51
John <ashton.basile@gmail.com>
2023-07-25 10:30:36 +0100

    I added a .env with a password for the database to the .gitignore file

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+PASSWORD = os.getenv('PASSWORD')
 
 app = Flask(__name__)
+# for new project, change 'tasks_app' to db name
+app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://postgres:{{PASSWORD}}@localhost:5432/tasks_app"
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True) # primary key will auto-increment id
+	first_name = db.Column(db.String(64))
+	last_name = db.Column(db.String(64))
+	tasks = db.relationship('Task', backref='user') # backref creates on the task a user property so you can do taks.user
+
+class Task(db.Model):
+	id = db.Column(db.Integer, primary_key=True) # primary key will auto-increment id
+	description = db.Column(db.Text())
+	duration = db.Column(db.Integer)
+	completed = db.Column(db.Boolean, default=False)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # table_name.primary_key_column
 
 @app.route("/")
 def hello_world():
